65e4994ba769cde732ea9e176ce36d531a7ea4ed
kevinnathang <116236277+kevinnathang@users.noreply.github.com>
2023-12-01 09:15:31 +0900

    Translate comments to Japanese, Fix calc最低品質保証価格 array index

--- psm.js
+++ psm.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 
 const args = process.argv.slice(2);
 
-// Check if the required arguments are provided at node command line
+//必要な引数がノードコマンドラインで提供されているかチェックする。
 if (args.length !== 2 || args[0] !== "--csvfile") {
   console.log(
-    `Please provide the CSV file using '--csvfile' argument. Make sure the end of the file contains ".csv"`
+    `引数 '--csvfile' でCSVファイルを指定してください。ファイルの末尾に".csv "が含まれていることを確認してください。`
   );
   process.exit(1);
 }
@@ -15,35 +15,35 @@ const csvFilePath = args[1];
 
 (async () => {
   try {
-    //Convert CSV file to JSON
+    //CSVファイルをJSONに変換する
     const dataArr = await csv().fromFile(csvFilePath);
 
-    //Get the maximum sample number
+    //最大サンプル数の取得
     const sampleNum = Math.max(...dataArr.map((obj) => obj["sample number"]));
 
-    //Get different arrays for each type of data for later calculation
+    //後で計算するために、データの種類ごとに異なる配列を取得する。
     const dataVeryHigh = dataArr.map((obj) => parseFloat(obj["高すぎる"]));
     const dataHigh = dataArr.map((obj) => parseFloat(obj["高い"]));
     const dataLow = dataArr.map((obj) => parseFloat(obj["安い"]));
     const dataTooLow = dataArr.map((obj) => parseFloat(obj["安すぎる"]));
 
-    //Function to calculate the number of values in a given array within a specified range (simulating excel COUNTIF) for high prices
+    //指定された配列の値が、指定された範囲内にある個数を計算する関数（エクセルのCOUNTIFをシミュレートする）。（高い値段）
     function calculateCountifHigh(array, limit) {
       return array.filter((data) => data <= limit).length / sampleNum;
     }
 
-    //Function to calculate the number of values in a given array within a specified range (simulating excel COUNTIF) for low prices
+    //指定された配列の値が、指定された範囲内にある個数を計算する関数（エクセルのCOUNTIFをシミュレートする）。（低い値段）
     function calculateCountifLow(array, limit) {
       return array.filter((data) => data >= limit).length / sampleNum;
     }
 
-    //Set differente arrays for the 4 types of prices
+    //4種類の価格に対して異なる配列を設定する。
     const expensive = [];
     const tooExpensive = [];
     const cheap = [];
     const tooCheap = [];
 
-    //Calculate count for 高い and 高すぎる
+    //高いと高すぎるのカウントを計算する
     for (let i = 50; i <= 600; i += 50) {
       const count = calculateCountifHigh(dataHigh, i);
       expensive.push({ key: i, value: count.toFixed(2) });
@@ -59,6 +59,9 @@ const csvFilePath = args[1];
       tooCheap.push({ key: i, value: count2.toFixed(2) });
     }
 
+    //EXCELのグラフに基づいて、関数の配列インデックスを決定する。
+    //線が250円と300円の間で交差する場合、インデックス4と5を使用。
+    //線が200円と250円の間で交差する場合は、インデックス3と4を使用する。
     function calc最高価格() {
       const C3 = `${tooExpensive[4].key}`;
       const D3 = `${tooExpensive[4].value}`;
@@ -81,15 +84,15 @@ const csvFilePath = args[1];
     }
 
     function calc妥協価格() {
-      const C3 = `${expensive[4].key}`;
-      const D3 = `${expensive[4].value}`;
-      const C4 = `${expensive[5].key}`;
-      const D4 = `${expensive[5].value}`;
+      const C3 = `${expensive[4].key}`; // x1
+      const D3 = `${expensive[4].value}`; // y1
+      const C4 = `${expensive[5].key}`; // x2
+      const D4 = `${expensive[5].value}`; // y2
 
-      const C5 = `${cheap[4].key}`;
-      const D5 = `${cheap[4].value}`;
-      const C6 = `${cheap[5].key}`;
-      const D6 = `${cheap[5].value}`;
+      const C5 = `${cheap[4].key}`; // x3
+      const D5 = `${cheap[4].value}`; // y3
+      const C6 = `${cheap[5].key}`; // x4
+      const D6 = `${cheap[5].value}`; // y4
 
       return console.log(
         `妥協価格: ${Math.round(
@@ -123,15 +126,15 @@ const csvFilePath = args[1];
     }
 
     function calc最低品質保証価格() {
-      const C3 = `${expensive[4].key}`;
-      const D3 = `${expensive[4].value}`;
-      const C4 = `${expensive[5].key}`;
-      const D4 = `${expensive[5].value}`;
+      const C3 = `${expensive[3].key}`;
+      const D3 = `${expensive[3].value}`;
+      const C4 = `${expensive[4].key}`;
+      const D4 = `${expensive[4].value}`;
 
-      const C5 = `${tooCheap[4].key}`;
-      const D5 = `${tooCheap[4].value}`;
-      const C6 = `${tooCheap[5].key}`;
-      const D6 = `${tooCheap[5].value}`;
+      const C5 = `${tooCheap[3].key}`;
+      const D5 = `${tooCheap[3].value}`;
+      const C6 = `${tooCheap[4].key}`;
+      const D6 = `${tooCheap[4].value}`;
 
       return console.log(
         `最低品質保証価格: ${Math.round(
@@ -147,6 +150,6 @@ const csvFilePath = args[1];
     calc妥協価格();
     calc最高価格();
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error("エラーが発生した:", error);
   }
 })();
